1e5648aa322ad98be066450257e32562f37017de
Filipe Castro <filipeoxs@gmail.com>
2023-07-10 00:09:50 -0300

    Configuração do script que informa quando algum usuário logou na VM

---
+++ script_que_informa_quem_logou/.bash_history
@@ -0,0 +1,25 @@
+ls
+ls -lha
+rm quemlogou.sh 
+fg
+touch script_teste.sh
+nano script_teste.sh 
+./script_teste.sh
+ls -lha
+exit
+exit
+ls
+nano quemlogou.sh 
+./quemlogou.sh 
+nano quemlogou.sh 
+./quemlogou.sh 
+ls -lha
+chown root:root quemlogou.sh 
+ls -lha
+./quemlogou.sh 
+sudo chown root:root quemlogou.sh 
+ls -lha
+su vagrant
+vagrant destroy
+exit
+exit
---
+++ script_que_informa_quem_logou/quemlogou.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Defina o diretório onde o arquivo .txt será armazenado
+LOG_FILE="/home/vagrant_script/login.txt"
+
+# Função para registrar o login no arquivo .txt
+function registrar_login {
+    echo "$(date) - Usuário $USER fez login" >> "$LOG_FILE"
+}
+
+# Execute a função registrar_login sempre que um usuário fizer login
+trap 'registrar_login' INT TERM EXIT
+
+# Defina o diretório na VM onde você deseja salvar o arquivo .txt
+LOCAL_DIR="/home/vagrant_script"
+
+# Certifique-se de que o diretório existe e tem as permissões adequadas
+mkdir -p "$LOCAL_DIR"
+chmod 777 "$LOCAL_DIR"
+
+# Loop infinito para manter o script em execução 24x7
+while true; do
+    # Aguarde 1 minuto antes de registrar novamente
+    sleep 1
+done
